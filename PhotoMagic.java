fad236a73749ec2a7cce04e45fb779720c05b0a7
pranavp <pranav.puranam@gmail.com>
2022-10-10 13:32:40 -0700

    Add files via upload

---
+++ PhotoMagic.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+
+public class PhotoMagic {
+
+    public static void main(String[] args) {
+        Picture original = new Picture(
+                "./pipe.png");
+        original.show();
+        Picture encrypted = transform(original, new LFSR("01101000010100010000", 16));
+        encrypted.show();
+    }
+
+    public static Picture transform(Picture pic, LFSR lfsr) {
+        Picture encoded = new Picture(
+                "./pipe.png");
+        for (int r = 0; r < pic.height(); r++) {
+            for (int c = 0; c < pic.width(); c++) {
+                Color color = pic.get(c, r);
+                Color transformed = new Color(color.getRed() ^ lfsr.generate(8), color.getGreen() ^ lfsr.generate(8),
+                        color.getBlue() ^ lfsr.generate(8));
+                encoded.set(c, r, transformed);
+            }
+        }
+        return encoded;
+    }
+
+}
---
+++ PhotoMagicDeluxe.java
@@ -0,0 +1,24 @@
+public class PhotoMagicDeluxe {
+
+    static String base64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+    public static Picture transform(Picture pic, String password, int tap) {
+        StringBuffer binary = new StringBuffer();
+        for (char c : password.toCharArray()) {
+            String shortBinary = Integer.toBinaryString(base64.indexOf(c));
+            binary.append("000000".substring(shortBinary.length()) + shortBinary);
+        }
+        System.out.println(binary);
+        PhotoMagic.transform(pic, new LFSR(binary.toString(), tap));
+        return pic;
+    }
+
+    public static void main(String[] args) {
+        Picture original = new Picture(
+                "./mystery.png");
+        original.show();
+        Picture encrypted = transform(original, "OPENSESAME", 58);
+        encrypted.show();
+    }
+
+}
